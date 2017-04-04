3525780c292d5ac6710e2f2dd5f46f6e1ffe95c3
Scott Preston <me@scottpreston.org>
2017-04-04 15:34:29 -0400

    adding bootstrap component (modal)

--- js/src/components/MyButton.js
+++ js/src/components/MyButton.js
@@ -3,29 +3,28 @@ import ReactDOM from 'react-dom';
 import {doItAction} from '../actions';
 import css from 'bootstrap/dist/css/bootstrap.css';
 import classnames from 'classnames';
+import MyModal from './MyModal';
 
 export default class MyButton extends React.Component {
 
   constructor() {
     super();
+    this.bootstrapModal = this.bootstrapModal.bind(this);
   }
 
-  // doIt() {
-  //   let {dispatch} = this.props;
-  //   dispatch(doItAction({}));
-  // }
-
-  do() {
-    this.props.doIt()
+  bootstrapModal() {
+     $('#MyModal').modal('show');
   }
 
   render() {
     let classes= classnames('btn','btn-success'); // if you wanted to use {classes}
-    return (      
+    return (
         <div>
+            <MyModal />
             <p>My Hello Prop : <strong>{this.props.hello}</strong>.</p>
             <button onClick={this.props.doIt} className={classes}>Populate Above</button>
             <button className="btn btn-primary">Sample Primary Button</button>
+            <button className="btn btn-warning" onClick={this.bootstrapModal}>Bootstrap Modal</button>
         </div>
     );
   }
